7c14433a6be95435b5f1a0618ba55d9d38b4bf32
paulov-t <pargsoft@hotmail.com>
2025-06-02 16:39:38 +0100

    add HomeController with Index and Ammo views

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Paulov.Tarkov.WebServer.DOTNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.AppVersion = Assembly.GetAssembly(typeof(HomeController)).GetName().Version;
+            ViewBag.ServerMode = this.configuration["ServerMode"].ToString();
+            return View();
+        }
+
+        [Route("/ammo")]
+        [HttpGet]
+        public IActionResult Ammo()
+        {
+            ViewBag.AppVersion = Assembly.GetAssembly(typeof(HomeController)).GetName().Version;
+            ViewBag.ServerMode = this.configuration["ServerMode"].ToString();
+            return View();
+        }
+    }
+}
