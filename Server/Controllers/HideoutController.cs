d82c8bbde5db5b39b036823da98bc86d6468330b
paulov-t <pargsoft@hotmail.com>
2025-06-02 16:38:29 +0100

    add HideoutController stub

---
+++ Server/Controllers/HideoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Paulov.Tarkov.WebServer.DOTNET.Controllers
+{
+    public class HideoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
