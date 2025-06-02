6ef33147785ed3775128aff53dd2d156a8eed71c
paulov-t <pargsoft@hotmail.com>
2025-06-02 16:39:00 +0100

    add QuestController stub

---
+++ Server/Controllers/QuestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Paulov.Tarkov.WebServer.DOTNET.Controllers
+{
+    public class QuestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
