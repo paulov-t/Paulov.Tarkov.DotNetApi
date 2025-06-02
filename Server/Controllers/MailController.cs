2a853f023009f0ef0d998af6b2d14a5a93ca2875
paulov-t <pargsoft@hotmail.com>
2025-06-02 16:38:41 +0100

    add MailController stub

---
+++ Server/Controllers/MailController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Paulov.Tarkov.WebServer.DOTNET.Controllers
+{
+    public class MailController : ControllerBase
+    {
+        [Route("client/mail/dialog/view")]
+        [HttpPost]
+        public async void DialogView()
+        {
+        }
+    }
+}
