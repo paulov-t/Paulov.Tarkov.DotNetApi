dd6777b5eba3fa2d94c406eacd89a9728af4569f
paulov-t <pargsoft@hotmail.com>
2025-06-16 23:06:11 +0100

    add localmatchendresponse model

---
+++ Libraries/Paulov.TarkovModels/Responses/LocalMatchEndResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Paulov.TarkovModels.Responses
+{
+    public sealed class LocalMatchEndResponse
+    {
+        [JsonProperty("serverId")]
+        public string ServerId { get; set; }
+
+        [JsonProperty("results")]
+        public JObject Results { get; set; }
+    }
+}
