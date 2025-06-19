4f968ff0aa565301d0086c963b7dfb42c61ed337
paulov-t <pargsoft@hotmail.com>
2025-06-19 15:11:18 +0100

    Entries are now stored using authorized calls to github api using a token to get all the files from a repository

--- Libraries/Paulov.TarkovServices/Providers/DatabaseProviders/CloudDatabaseProviders/GitHubDatabaseProvider.cs
+++ Libraries/Paulov.TarkovServices/Providers/DatabaseProviders/CloudDatabaseProviders/GitHubDatabaseProvider.cs
@@ -7,23 +7,42 @@ using System.Text;
 
 namespace Paulov.TarkovServices.Providers.DatabaseProviders.CloudDatabaseProviders
 {
+    /// <summary>
+    /// Provides access to a GitHub-hosted database by interacting with raw content files.
+    /// </summary>
+    /// <remarks>This class connects to a GitHub repository and retrieves database entries stored as raw
+    /// files. It implements the <see cref="IDatabaseProvider"/> interface, allowing integration with systems that
+    /// require database-like functionality.
+    /// 
+    /// This has the requirement for configuration to include a GitHub repository URL in the format "username/repository" stored in appsettings.json as GitHubDatabaseUrl.
+    /// This has the requirement for configuration to include a GitHub Authorization stored in appsettings.json as GitHubAuthToken.
+    /// 
+    /// 
+    /// 
+    /// </remarks>
+    /// 
     public sealed class GitHubDatabaseProvider : IDatabaseProvider
     {
-        public List<EntryModel> Entries { get; set; } = new List<EntryModel>();
+        public List<EntryModel> Entries { get; } = new List<EntryModel>();
 
-        HttpClient client;
+        HttpClient _client;
 
+        IConfiguration _configuration;
+
+        string _connectionString;
 
         public GitHubDatabaseProvider(IConfiguration configuration)
         {
+            this._configuration = configuration;
             Connect(configuration["GitHubDatabaseUrl"]);
         }
 
         public void Connect(string connectionString)
         {
-            client = new HttpClient();
-            client.BaseAddress = new Uri($"https://raw.githubusercontent.com/{connectionString}/refs/heads/master/");
-            client.GetAsync("database/globals.json").ContinueWith(task =>
+            _connectionString = connectionString;
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri($"https://raw.githubusercontent.com/{connectionString}/refs/heads/master/");
+            _client.GetAsync("database/globals.json").ContinueWith(task =>
             {
                 if (task.IsCompletedSuccessfully && task.Result.IsSuccessStatusCode)
                 {
@@ -32,6 +51,40 @@ namespace Paulov.TarkovServices.Providers.DatabaseProviders.CloudDatabaseProvide
                     Entries.Add(new EntryModel("globals.json", "database/globals.json", this));
                 }
             });
+
+
+            var apiEntries = new HttpClient();
+            apiEntries.BaseAddress = new Uri($"https://api.github.com/");
+            apiEntries.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _configuration["GitHubAuthToken"]);
+            apiEntries.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("*/*"));
+            apiEntries.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("Paulov.TarkovServices", "1.0"));
+            var r = apiEntries.GetAsync($"/repos/{connectionString}/contents").Result.Content.ReadAsStringAsync().Result;
+            RecursiveAddEntries(apiEntries, JArray.Parse(r));
+            _ = Entries;
+        }
+
+        /// <summary>
+        /// Recursively adds entries to the Entries list from the provided JArray of entries. This is based on the GitHub API response structure.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="entries"></param>
+        private void RecursiveAddEntries(HttpClient client, JArray entries)
+        {
+            foreach (var entry in entries)
+            {
+                if (entry["type"].ToString() == "file")
+                {
+                    var name = entry["name"].ToString();
+                    var fullName = entry["path"].ToString();
+                    Entries.Add(new EntryModel(name, fullName, this));
+                }
+                else if (entry["type"].ToString() == "dir")
+                {
+                    var r = client.GetAsync($"/repos/{_connectionString}/contents/{entry["path"]}").Result.Content.ReadAsStringAsync().Result;
+                    if (r.StartsWith("["))
+                        RecursiveAddEntries(client, JArray.Parse(r));
+                }
+            }
         }
 
         public void Disconnect()
@@ -49,7 +102,7 @@ namespace Paulov.TarkovServices.Providers.DatabaseProviders.CloudDatabaseProvide
 
         public Stream GetEntryStream(string entryName)
         {
-            var response = client.GetAsync($"{entryName}").GetAwaiter().GetResult().Content.ReadAsStringAsync().Result;
+            var response = _client.GetAsync($"{entryName}").GetAwaiter().GetResult().Content.ReadAsStringAsync().Result;
 
             return new MemoryStream(Encoding.UTF8.GetBytes(response));
         }
