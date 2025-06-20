1ee8f2344dbeb196f3aa71ca9d6bd145da34ab78
paulov-t <pargsoft@hotmail.com>
2025-06-20 18:52:38 +0100

    Generate items randomly in containers

---
+++ Libraries/Paulov.TarkovServices/Helpers/ContainerHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+
+namespace Paulov.TarkovServices.Helpers
+{
+    public sealed class ContainerHelpers
+    {
+        /// <summary>
+        /// TODO: FIXME: This is purely pseudo code to get something in the containers and needs removing as it wont handle anything larger than 1x
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="itemWidth"></param>
+        /// <param name="itemHeight"></param>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
+        public bool PlaceItemInRandomSpotInContainer(bool[,] container, int itemWidth, int itemHeight, out Vector2 position, out bool rotation)
+        {
+            rotation = false;
+            position = new Vector2(-1, -1); // Default to an invalid position
+            var placed = false;
+            int x = 0, y = 0;
+            var containerY = container.GetLength(0);
+            var containerX = container.GetLength(1);
+            int attempts = 0;
+            do
+            {
+                x = Random.Shared.Next(0, containerX);
+                y = Random.Shared.Next(0, containerY);
+                if (container[y, x]) continue; // Skip if the position is already occupied
+
+                // Check if the position is within the container bounds
+                if (x < 0 || x >= containerX || y < 0 || y >= containerY)
+                    continue;
+
+                placed = true;
+                container[y, x] = true; // Mark the position as occupied
+                position = new Vector2(x, y);
+            }
+            while (!placed && attempts < 10);
+
+            return placed;
+
+        }
+    }
+}
--- Libraries/Paulov.TarkovServices/Services/LootGenerationService.cs
+++ Libraries/Paulov.TarkovServices/Services/LootGenerationService.cs
@@ -1,7 +1,10 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using EFT;
+using Newtonsoft.Json.Linq;
+using Paulov.TarkovServices.Helpers;
 using Paulov.TarkovServices.Providers.Interfaces;
 using Paulov.TarkovServices.Services.Interfaces;
 using System.Diagnostics;
+using System.Numerics;
 
 namespace Paulov.TarkovServices.Services
 {
@@ -29,7 +32,7 @@ namespace Paulov.TarkovServices.Services
                 throw new ArgumentNullException(nameof(location), "Location cannot be null.");
             }
 
-            Debug.WriteLine($"Generating loot for location {location["_id"]}.");
+            Debug.WriteLine($"Generating loot for location {location["Id"]}.");
 
             JArray lootItems = new JArray();
 
@@ -66,28 +69,101 @@ namespace Paulov.TarkovServices.Services
                                 , JObject.Parse(staticLootDocument.RootElement.GetRawText())[containerId]
                                 , locationLootChanceModifierFromFile
                                 , location["Name"].ToString()
+                                , itemTemplates
                                 ))
                                 lootItems.Add(item);
                         }
                         break;
                 }
             }
-            //foreach (var container in staticContainersDocument["staticContainers"])
-            //{
-            //}
 
             return JArray.FromObject(lootItems);
         }
 
-        private JArray GenerateContainerLoot(JToken containerTemplate, JToken staticLootForContainer, float locationLootChanceModifierFromFile, string locationName)
+        private JArray GenerateContainerLoot(JToken containerData, JToken staticLootForContainer, float locationLootChanceModifierFromFile, string locationName, JObject templateItemList)
         {
-            if (containerTemplate == null || staticLootForContainer == null)
+            if (containerData == null || staticLootForContainer == null)
             {
                 throw new ArgumentNullException("Container template or static loot for container cannot be null.");
             }
 
             JArray lootItems = new JArray();
-            lootItems.Add(containerTemplate);
+
+            var containerLootItems = containerData["Items"] as JArray;
+            var containerParentId = containerLootItems.First["_id"].ToString();
+            var containerTemplateId = containerLootItems.First["_tpl"].ToString();
+
+            var isWeaponBox = (containerTemplateId == "5909d5ef86f77467974efbd8");
+
+            var containerTemplate = templateItemList[containerTemplateId];
+            var containerTemplateProperties = containerTemplate["_props"];
+            var containerTemplateGrid = ((JArray)containerTemplateProperties["Grids"])[0];
+            bool[,] container2d = new bool[int.Parse(containerTemplateGrid["_props"]["cellsV"].ToString()), int.Parse(containerTemplateGrid["_props"]["cellsH"].ToString())];
+            foreach (var item in GenerateContainerLootList(containerParentId, container2d, staticLootForContainer, templateItemList))
+            {
+                //containerLootItems.Add(containerData);
+                containerLootItems.Add(item);
+            }
+            containerData["Items"] = containerLootItems;
+
+            lootItems.Add(containerData);
+            return lootItems;
+        }
+
+        private JArray GenerateContainerLootList(string containerParentId, bool[,] container2d, JToken staticLootForContainer, JObject templateItemList)
+        {
+            JArray lootItems = new JArray();
+
+            var itemCountDistList = staticLootForContainer["itemcountDistribution"] as JArray;
+            var itemCount = Math.Max(1, itemCountDistList[this.Randomizer.Next(0, itemCountDistList.Count - 1)].Count());
+            var itemDistList = staticLootForContainer["itemDistribution"] as JArray;
+
+            var orderedDistList = itemDistList
+                .OrderByDescending(x => float.Parse(x["relativeProbability"].ToString()))
+                .Select(x => x)
+                .ToList();
+
+            var maxProbability = int.Parse(orderedDistList[0]["relativeProbability"].ToString());
+            var lowestProbability = int.Parse(orderedDistList[orderedDistList.Count - 1]["relativeProbability"].ToString());
+
+            for (var i = 0; i < orderedDistList.Count && lootItems.Count < itemCount; i++)
+            {
+                var item = orderedDistList[i];
+                var itemRelativeProbability = int.Parse(orderedDistList[0]["relativeProbability"].ToString());
+                var calculatedProbability = Randomizer.Next((int)Math.Round(itemRelativeProbability * 0.9), (int)Math.Round(itemRelativeProbability * 1.5));
+                // TODO: Calculate by rarity
+                if (calculatedProbability < maxProbability)
+                {
+                    var templateItem = templateItemList[item["tpl"].ToString()];
+                    var itemWidth = int.Parse(templateItem["_props"]["Width"].ToString());
+                    var itemHeight = int.Parse(templateItem["_props"]["Height"].ToString());
+
+                    if (new ContainerHelpers().PlaceItemInRandomSpotInContainer(container2d, itemWidth, itemHeight, out Vector2 position, out bool rotation))
+                    {
+                        JObject resultItem = new JObject();
+                        resultItem["__DEBUG_Name"] = templateItem["_name"];
+                        resultItem["_id"] = MongoID.Generate(false).ToString();
+                        resultItem["_tpl"] = item["tpl"].ToString();
+                        resultItem["parentId"] = containerParentId;
+                        resultItem["location"] = new JObject();
+                        resultItem["location"]["x"] = (int)Math.Round(position.X);
+                        resultItem["location"]["y"] = (int)Math.Round(position.Y);
+                        resultItem["location"]["r"] = (rotation ? 1 : 0);
+                        resultItem["slotId"] = "main";
+
+                        if (templateItem["_parent"].ToString() == "543be5dd4bdc2deb348b4569" || templateItem["_parent"].ToString() == "5485a8684bdc2da71d8b4567")
+                        {
+                            var stackMin = int.Parse(templateItem["_props"]["StackMinRandom"].ToString());
+                            var stackMax = int.Parse(templateItem["_props"]["StackMaxRandom"].ToString());
+                            var stackCount = Random.Shared.Next(stackMin, stackMax);
+                            resultItem["upd"] = JToken.FromObject(new { StackObjectsCount = stackCount });
+                        }
+
+                        lootItems.Add(resultItem);
+                    }
+                }
+            }
+
 
             return lootItems;
         }
