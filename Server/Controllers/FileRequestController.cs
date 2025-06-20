edd7904989a7289f8247fad18aaa167a8348d4aa
paulov-t <pargsoft@hotmail.com>
2025-06-20 14:09:09 +0100

    Remove files.zip and switch to redirecting files to Paulov.Tarkov.Db

--- Server/Controllers/FileRequestController.cs
+++ Server/Controllers/FileRequestController.cs
@@ -1,5 +1,4 @@
-﻿using System.IO.Compression;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 
 namespace Paulov.Tarkov.WebServer.DOTNET.Controllers;
@@ -7,34 +6,14 @@ namespace Paulov.Tarkov.WebServer.DOTNET.Controllers;
 /// <summary>
 /// Handles all client requests for files from the server
 /// </summary>
-/// <param name="zipArchive">The <see cref="ZipArchive"/> to be injected</param>
 [Route("files/{*path}")]
-public class FileRequestController([FromKeyedServices("fileAssets")] ZipArchive zipArchive) : Controller
+public class FileRequestController() : Controller
 {
     private readonly FileExtensionContentTypeProvider _contentTypeProvider = new();
-    /// <summary>
-    /// Serves the requested file from the <see cref="ZipArchive"/> if a <see cref="ZipArchiveEntry"/> exists
-    /// </summary>
-    /// <param name="path">The <see cref="string"/> path to look for the request file</param>
-    /// <returns>
-    /// A <see cref="Task{IActionResult}"/> that yields a <see cref="FileStreamResult"/> if the file is found,
-    /// or a <see cref="NotFoundResult"/> if not.
-    /// </returns>
-    [HttpGet]
-    public Task<IActionResult> ServeFile(string path)
-    {
-        //TODO: Look for ways to not need this. Without this lock, if two threads access the archive it throws exceptions
-        lock (zipArchive)
-        {
-            ZipArchiveEntry archiveEntry = zipArchive.GetEntry(path);
-            if(archiveEntry == null) return Task.FromResult<IActionResult>(NotFound());
 
-            if (!_contentTypeProvider.TryGetContentType(archiveEntry.Name, out string contentType))
-            {
-                contentType = "application/octet-stream";
-            }
-        
-            return Task.FromResult<IActionResult>(File(archiveEntry.Open(), contentType, archiveEntry.Name));   
-        }
+    [HttpGet]
+    public IActionResult ServeFile(string path)
+    {
+        return new RedirectResult($"https://raw.githubusercontent.com/paulov-t/Paulov.Tarkov.Db/refs/heads/master/files/{path}");
     }
 }
