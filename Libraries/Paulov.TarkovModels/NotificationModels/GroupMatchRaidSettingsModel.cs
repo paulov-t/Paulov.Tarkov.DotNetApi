46ecc59f20c6795247cebcaaf5cbbd1db9b14321
paulov-t <pargsoft@hotmail.com>
2025-07-05 20:23:19 +0100

    Add notification models for stronger and better explained types

---
+++ Libraries/Paulov.TarkovModels/NotificationModels/GroupMatchRaidSettingsModel.cs
@@ -0,0 +1,19 @@
+﻿using EFT;
+using EFT.Communications;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Paulov.TarkovModels.NotificationModels
+{
+    public sealed class GroupMatchRaidSettingsModel : BaseNotificationModel
+    {
+        [JsonProperty("raidSettings")]
+        [JsonPropertyName("raidSettings")]
+        public RaidSettings? RaidSettings { get; set; }
+
+        public GroupMatchRaidSettingsModel(RaidSettings? raidSettings) : base(ENotificationType.GroupMatchRaidSettings)
+        {
+            RaidSettings = raidSettings ?? throw new ArgumentNullException(nameof(raidSettings), "RaidSettings cannot be null.");
+        }
+    }
+}
