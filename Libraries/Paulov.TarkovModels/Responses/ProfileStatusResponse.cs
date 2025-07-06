52e554dba169e59658213fc1ffca1cf94d34c069
paulov-t <pargsoft@hotmail.com>
2025-07-06 19:48:00 +0100

    Add ProfileStatusResponse response

---
+++ Libraries/Paulov.TarkovModels/Responses/ProfileStatusResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Paulov.TarkovModels.Responses
+{
+    public sealed class ProfileStatusResponse
+    {
+        public bool maxPveCountExceeded { get; set; } = false;
+
+        public List<ProfileStatusModel> profiles { get; set; } = new();
+
+        public ProfileStatusResponse() { }
+
+        public ProfileStatusResponse(bool maxPveCountExceeded, List<ProfileStatusModel> profiles)
+        {
+            this.maxPveCountExceeded = maxPveCountExceeded;
+            this.profiles = profiles;
+        }
+    }
+}
