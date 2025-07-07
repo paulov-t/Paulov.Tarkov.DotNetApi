5ac2c625b5716e372531464245eeeefb1a6b119f
paulov-t <pargsoft@hotmail.com>
2025-07-07 23:23:28 +0100

    Create constructor that handles the expected order of profiles

--- Libraries/Paulov.TarkovModels/Responses/ProfileStatusResponse.cs
+++ Libraries/Paulov.TarkovModels/Responses/ProfileStatusResponse.cs
@@ -4,6 +4,9 @@
     {
         public bool maxPveCountExceeded { get; set; } = false;
 
+        /// <summary>
+        /// The profiles are expected by the client in the following format: 0: Scav, 1 PMC
+        /// </summary>
         public List<ProfileStatusModel> profiles { get; set; } = new();
 
         public ProfileStatusResponse() { }
@@ -12,6 +15,15 @@
         {
             this.maxPveCountExceeded = maxPveCountExceeded;
             this.profiles = profiles;
+        }
+
+        public ProfileStatusResponse(bool maxPveCountExceeded, ProfileStatusModel pmcStatusModel, ProfileStatusModel scavStatusModel)
+        {
+            this.maxPveCountExceeded = maxPveCountExceeded;
+            this.profiles = new List<ProfileStatusModel>()
+            {
+                scavStatusModel, pmcStatusModel
+            };
         }
     }
 }
