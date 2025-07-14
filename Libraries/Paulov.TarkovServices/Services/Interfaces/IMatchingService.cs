70bf6f93f96b3151f180f8ce7af7166ccf31d36b
paulov-t <pargsoft@hotmail.com>
2025-07-14 22:24:37 +0100

    Add new MatchingService

---
+++ Libraries/Paulov.TarkovServices/Services/Interfaces/IMatchingService.cs
@@ -0,0 +1,19 @@
+﻿using Paulov.TarkovModels.GroupingModels;
+
+namespace Paulov.TarkovServices.Services.Interfaces
+{
+    public interface IMatchingService
+    {
+        public List<MatchingGroup> MatchingGroups { get; }
+        public MatchingGroup CreateMatchingGroupBySessionId(string sessionId);
+        public MatchingGroup GetMatchingGroupByRequestId(string requestId);
+        public MatchingGroup GetMatchingGroupBySessionId(string sessionId);
+        public MatchingGroup GetMatchingGroupByAID(string aid);
+        public void DeleteMatchingGroupBySessionId(string sessionId);
+        public MatchingGroup SendInviteToUser(string requestId, string mySessionId, string toSessionId);
+        public MatchingGroup AcceptInviteFromUser(string mySessionId);
+        public bool? CancelInviteToUser(string inviteRequestId);
+
+
+    }
+}
